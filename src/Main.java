88493bc7d8c93b9cced11850c3b0e8d2c67f1b24
student <you@example.com>
2018-07-20 14:56:46 -0500

    check in

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	      Ex1.
+//        System.out.println("its a great day today");
+//        System.out.print("Enter your name:");
+//          Scanner scanIn = new Scanner(System.in);
+//         String name = scanIn.nextLine();
+//        System.out.println("hello:"+name);
+
+//         Ex2.
+//         Scanner in = new Scanner(System.in);
+//        System.out.print("Input 1st integer: ");
+//        int firstInt = in.nextInt();
+//        System.out.print("Input 2nd integer: ");
+//        int secondInt = in.nextInt();
+//
+//        System.out.printf("Sum of two integers: %d%n", firstInt + secondInt);
+//        System.out.printf("Difference of two integers: %d%n", firstInt - secondInt);
+//
+//////            Ex3.
+//                  Scanner in = new Scanner(System.in);
+//                 System.out.print("balance: ");
+//                 int firstInt = in.nextInt();
+//                 System.out.print("ive added money: ");
+//                 int secondInt = in.nextInt();
+//
+//                 System.out.printf("Sum of two integers: %d%n", firstInt + secondInt);
+
+
+
+    }
+}
